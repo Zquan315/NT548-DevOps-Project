fd3b14d2527e17ad81b45ff65fca015d84c8a8f8
Zquan3101 <22521190@gm.uit.edu.vn>
2025-04-22 22:33:13 +0700

    Config security by using .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentsModule } from './students/students.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://quantc:31012004@nt548-devops.68ujgah.mongodb.net/'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017'),
     StudentsModule,
   ],
 })
